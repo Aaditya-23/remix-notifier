372d84eed84ed697103dccb098aa400eff2d8d83
Aaditya Verma <aaditya220055@gmail.com>
2023-03-27 19:01:04 +0530

    added navbar and styles

---
+++ app/components/navbar.tsx
@@ -0,0 +1,13 @@
+import { AiOutlineGithub } from 'react-icons/ai';
+
+export default function Navbar() {
+  return (
+    <nav className='p-2 flex justify-between items-center'>
+      <h1 className='text-2xl font-semibold capitalize'>remix-notifier</h1>
+
+      <a href='#' className='text-lg'>
+        <AiOutlineGithub color='white' />
+      </a>
+    </nav>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
+import { Navbar } from '~/components';
+
 export default function Index() {
-  return <div>Getting started</div>;
+  return (
+    <div className='w-full min-h-screen bg-gradient-to-r from-[#8a2387] via-[#e94057] to-[#f27121]'>
+      <Navbar />  
+    </div>
+  );
 }
